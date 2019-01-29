da019f4299f7412b29d3d92db721ac4632498a50
Christine Cua <cloudycua@gmail.com>
2019-01-29 18:06:57 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,7 +28,7 @@ function ternaryTeenager(age){
   return age >= 13 && age <=19 ? "You are a teenager" : "You are not a teenager"
 }
 
-switch (age) {
+switchAge(age) {
   case 13:
   case 14:
   case 15:
