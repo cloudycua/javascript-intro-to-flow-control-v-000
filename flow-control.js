1cc3f337333a0be54918c5b2315bde536d51b6a6
Christine Cua <cloudycua@gmail.com>
2019-01-29 18:02:46 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -31,29 +31,25 @@ function ternaryTeenager(age){
 switch (age) {
   case 13:
     console.log("You are a teenager");
-  case 13:
+    break;
+  case 14:
     console.log("You are a teenager");
-    case 13:
-      console.log("You are a teenager");
-      break;
-    case 14:
-      console.log("You are a teenager");
-      break;
-    case 15:
-      console.log("You are a teenager");
-      break;
-    case 16:
-      console.log("You are a teenager");
-      break;
-    case 17:
-      console.log("You are a teenager");
-      break;
-    case 18:
-      console.log("You are a teenager");
-      break;
-    case 19:
-      console.log("You are a teenager");
-      break;
-    default:
-      console.log("You have an age")
+    break;
+  case 15:
+    console.log("You are a teenager");
+    break;
+  case 16:
+    console.log("You are a teenager");
+    break;
+  case 17:
+    console.log("You are a teenager");
+    break;
+  case 18:
+    console.log("You are a teenager");
+    break;
+  case 19:
+    console.log("You are a teenager");
+    break;
+  default:
+    console.log("You have an age")
 }
